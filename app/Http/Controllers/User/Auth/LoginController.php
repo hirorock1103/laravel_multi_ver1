7ce46864bde46463013095ebfa0bc1f6e4b0264f
hirorock1103 <mdiz1103@gmail.com>
2021-04-05 09:09:16 +0900

    User:ログイン周り処理

--- app/Http/Controllers/User/Auth/LoginController.php
+++ app/Http/Controllers/User/Auth/LoginController.php
@@ -37,4 +37,32 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    // Guardの認証方法を指定
+    protected function guard()
+    {
+        return Auth::guard('user');
+    }
+
+    // ログイン画面
+    public function showLoginForm()
+    {
+        return view('user.auth.login');
+    }
+
+    // ログアウト処理
+    public function logout(Request $request)
+    {
+        Auth::guard('user')->logout();
+
+        return $this->loggedOut($request);
+    }
+
+    // ログアウトした時のリダイレクト先
+    public function loggedOut(Request $request)
+    {
+        return redirect(route('user.login'));
+    }
+
+
 }
