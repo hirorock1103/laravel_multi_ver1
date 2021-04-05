11d953732747810bde449c916d557a6929e4e4f5
hirorock1103 <mdiz1103@gmail.com>
2021-04-05 10:08:28 +0900

    [mod]モデル名修正

--- app/Http/Controllers/Admin/Auth/RegisterController.php
+++ app/Http/Controllers/Admin/Auth/RegisterController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use App\Models\User;
+use App\Models\Admin;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -71,7 +71,7 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\User
+     * @return \App\Admin
      */
     protected function create(array $data)
     {
