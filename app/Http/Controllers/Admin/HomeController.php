28acfc313bb6d6658cffdf19d33b77077154c5e6
hirorock1103 <mdiz1103@gmail.com>
2021-04-05 09:03:29 +0900

    homecontroller編集

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -7,15 +7,18 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
     public function index()
     {
-        //
+        return view('admin.home');
     }
+
+
 
     /**
      * Show the form for creating a new resource.
